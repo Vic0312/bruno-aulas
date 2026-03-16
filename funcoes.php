2ee48bdd8deaf585d9c5137a82b4ad028d3ad899
Vic0312 <vitoriabrito0317@gmail.com>
2026-03-16 16:09:46 -0300

    Commit 6

--- funcoes.php
+++ funcoes.php
@@ -7,4 +7,12 @@
     function sub($num1, $num2){
         return($num1 - $num2);
     }
+
+    function mult($num1, $num2){
+        return($num1 * $num2);
+    }
+
+    function div($num1, $num2){
+        return($num1 / $num2);
+    }
 ?>
